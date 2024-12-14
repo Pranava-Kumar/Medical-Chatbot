c3a5a51d85f30ed089d26ae76f8402b3b1a24c89
Pranava Kumar <pranavakumar.it@gmail.com>
2024-12-15 00:30:00 +0530

    Folder Structure Created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+  name='Generative AI Project',
+  version='0.0.0',
+  author='Pranava Kumar',
+  author_email='pranavakumar.it@gmail.com',
+  packages=find_packages(),
+  install_requires=[]
+)
